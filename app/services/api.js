104695ff216dfcd0e3061c5d4dc4c96062d28503
hammad257 <azamhamad77@gmail.com>
2025-07-25 03:56:20 +0500

    add env

--- app/services/api.js
+++ app/services/api.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = 'http://localhost:5000/api';
-
+const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:5000/api';
 
 const axiosInstance = axios.create({
     baseURL:BASE_URL
